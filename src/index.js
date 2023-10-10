dcc261774d620718b1fe80e0df4b2e83eec1b621
Serhii-Ruhtik <fopgavrilyk1@gmail.com>
2023-10-10 23:29:18 +0300

    asd

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 console.log(10);
+
+fetch('https://rickandmortyapi.com/api/character')
+  .then(response => {
+    console.log(response);
+    if (!response.ok) {
+      throw new Error(`Вимушена помилка статусу: ${response.status}`);
+      
+    }
+    // console.log(response.json());
+    return response.json();
+  })
+  .then(data => console.log(data))
+  .catch((err) => console.log(err));
